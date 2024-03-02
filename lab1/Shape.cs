e0fccc1a00cee258ff9e52beeb1c9bd0d7ba613b
quantum32043 <quantum32043@gmail.com>
2024-03-02 12:22:27 +0300

    1

--- lab1/Shape.cs
+++
@@ -1,9 +0,0 @@
-using System.Drawing;
-
-public abstract class Shape
-{
-    protected Color Color { get; set; } 
-    protected Point Location { get; set; } 
-
-    public abstract void Draw(Graphics graphics);
-}
